69e8354d14f48732396ed7645bb089d6b1cd8c25
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-09-30 16:52:00 +0300

    basic player post & get operations boilerplate

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,12 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose'
+import { InjectModel } from '@nestjs/mongoose'
+import { Player } from './interfaces/player.interface'
 import { Stat } from './interfaces/stat.interface'
+import { CreatePlayerDTO } from './dto/create-player.dto';
 
 let newStat: Stat = {id: 1, text: 'benis'}
 console.log(newStat)
 
 @Injectable()
 export class StatsService {
-  private readonly stats: Stat[] = [];
+  constructor(@InjectModel('Player') private readonly playerModel: Model<Player>) {}
+
+    async addPlayer(createPlayerDTO: CreatePlayerDTO): Promise<Player> {
+      const newPlayer = await this.playerModel(createPlayerDTO);
+      return newPlayer.save();
+    }
+    
+    async getPlayer(playerID): Promise<Player>{
+      const player = await this.playerModel.findById(playerID).exec()
+      return player
+    }
+
+    async getPlayers(): Promise<Player[]> {
+      const players = await this.playerModel.find().exec()
+      return players
+    }
+  }
+
+
+  /*private readonly stats: Stat[] = [];
 
   create(stat: Stat){
     this.stats.push(stat);
@@ -18,5 +41,4 @@ export class StatsService {
 
   getHello(): string {
     return 'Hello World!';
-  }
-}
+  }*/
